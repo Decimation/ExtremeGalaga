48a1f1aa2f9b120548b8393632a769bcb543f42b
HathawayL <31742873+HathawayL@users.noreply.github.com>
2018-05-30 12:56:49 -0500

    g

---
+++ src/main/java/com/deci/galaga/Alien2.java
@@ -0,0 +1,62 @@
+package com.deci.galaga;
+
+public class Alien2 extends GObject {
+    private static final String IMG_URL = "https://raw.githubusercontent.com/jsvana/galaga/master/assets/images/enemy1.png";
+
+
+    Alien2() {
+        super(IMG_URL);
+        super.setY(100);
+        super.setX(10);
+        isAlive=true;
+
+    }
+
+    boolean isAlive() {
+        return isAlive;
+    }
+
+
+    void draw() {
+        GalagaEngine.instance.image(this.getGameImg(), this.getX(), this.getY());
+    }
+
+    @Override
+    void update() {
+
+
+    }
+
+    @Override
+    void manifest() {
+        if (isAlive)
+            super.manifestInternal(this);
+    }
+
+    @Override
+    void move(MovementTypes mt) {
+
+    }
+
+    @Override
+    void handleKey(final char c) {
+
+    }
+
+    void explode() {
+        SequentialImage.create(Assets.EG_GITHUB_ASSETS_ROOT, "explosion_f2.png", "explosion_f3.png", "explosion_f4.png").animate(getPoint());
+    }
+
+    @Override
+    void destroy() {
+        if (isAlive) {
+            getSound().play();
+            isAlive = false;
+
+
+        }
+
+
+    }
+    }
+}
